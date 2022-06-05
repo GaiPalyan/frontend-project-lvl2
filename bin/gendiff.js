80eb87a2b34eb551a7e5593859966ff1af1ec8a8
gai <chadreed020788@yandex.ru>
2022-06-05 10:14:23 +0300

    init project

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import {program} from "commander";
+
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.\n')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .parse();
+console.log(program)
+
